084972b7680425ddd39907d7e9c620e9f051283f
lToBContinued <wt3212834434@outlook.com>
2025-06-18 16:11:53 +0800

    感受diff算法

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import h from './mysnabbdom/h.js'
+import { init } from 'snabbdom/init'
+import { classModule } from 'snabbdom/modules/class'
+import { propsModule } from 'snabbdom/modules/props'
+import { styleModule } from 'snabbdom/modules/style'
+import { eventListenersModule } from 'snabbdom/modules/eventlisteners'
+import { h } from 'snabbdom/h' // helper function for creating vnodes
 
-const myVnode1 = h('div', {}, [
-  h('p', {}, '哈哈'),
-  h('p', {}, '嘻嘻'),
-  h('p', {}, '呵呵'),
-  h('p', {}, h('span', {}, 'A')),
+// 创建出patch函数
+const patch = init([classModule, propsModule, styleModule, eventListenersModule])
+const container = document.getElementById('container')
+const btn = document.getElementById('btn')
+const vnode1 = h('div', {}, [
+  h('p', { key: 'A' }, 'A'),
+  h('p', { key: 'B' }, 'B'),
+  h('p', { key: 'C' }, 'C'),
+  h('p', { key: 'D' }, 'D'),
 ])
-const myVnode2 = h('ul', {}, [
-  h('li', {}, '苹果'),
-  h('li', {}, '西瓜'),
-  h('li', {}, [h('div', {}, [h('div', {}, '哈哈'), h('div', {}, '嘻嘻')])]),
-  h('li', {}, h('p', {}, '火龙果')),
+patch(container, vnode1)
+
+const vnode2 = h('div', {}, [
+  h('p', { key: 'B' }, 'B'),
+  h('p', { key: 'A' }, 'A'),
+  h('p', { key: 'D' }, 'D'),
+  h('p', { key: 'C' }, 'C'),
 ])
-console.log('>>>>> file: index.js ~ method:  <<<<<\n', myVnode2)
+// 点击按钮时，将vnode1变为vnode2
+btn.onclick = () => {
+  patch(vnode1, vnode2)
+}
